fc71aa103e334cdba2bad3cbb1dfbdd67136071f
Mukesh Murugan <iammukeshm@gmail.com>
2020-07-18 01:23:40 +0530

    updated packages

---
+++ Customer.Microservice/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer.Microservice.Entities
+{
+    public class BaseEntity
+    {
+        public virtual int Id { get; set; }
+    }
+}
---
+++ Customer.Microservice/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer.Microservice.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+    }
+}
