0bba41812fed74ea25b441838e4a90cdb412d326
Mukesh Murugan <iammukeshm@gmail.com>
2020-07-18 19:30:35 +0530

    ocelot settings for product and customer

--- Gateway.WebApi/Program.cs
+++ Gateway.WebApi/Program.cs
@@ -16,17 +16,17 @@ namespace Gateway.WebApi
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                config
-                .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-                .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-            });
+public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        })
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config
+        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+        .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+    });
     }
 }
--- Gateway.WebApi/Startup.cs
+++ Gateway.WebApi/Startup.cs
@@ -16,26 +16,24 @@ namespace Gateway.WebApi
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddOcelot();
-        }
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddOcelot();
+}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-            await app.UseOcelot();
-        }
+public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    app.UseRouting();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
+    await app.UseOcelot();
+}
     }
 }
